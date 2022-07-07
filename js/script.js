a31efe237b86b09d7583f290f48c1c556f985e9b
vincemarq01 <38157522+vincemarq01@users.noreply.github.com>
2022-07-07 20:36:52 +0800

    Add initial cv website files

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+$(window).ready(function(){
+    var typed = new Typed(".typing", {
+        strings: ["Developer", "SoftWare Engineer" ],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+    });
+}
+
+
+  
+  );
